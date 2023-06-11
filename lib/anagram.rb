61470dda9a97770b591fbf84b20a48dc2fabc555
Aarow Epstein <aarowepstein1@gmail.com>
2023-06-11 13:24:54 -0700

    commit tree f380815560a99c23382064c87ce0b49bcaee2946

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+ def initialize(word)
+    @word = word
+ end
+
+ def match(str_array)
+    str_array.filter do |i|
+        i.chars.sort == @word.chars.sort
+    end
+ end
+end
+
+
+
+
+
